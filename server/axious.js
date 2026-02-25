101a2bfef299808754ebf5955592f00741d74db9
sagardhondge <sagardhondge56@gmail.com>
2026-02-26 00:08:08 +0530

    update in the server url

--- server/axious.js
+++ server/axious.js
@@ -1,6 +1,6 @@
 const token = localStorage.getItem("token");
 
-await axios.put("https://chatverse-backend-0c8u.onrender.com/api/user/profile", formData, {
+await axios.put("https://chat-verse-app.onrender.com/api/user/profile", formData, {
   headers: {
     Authorization: `Bearer ${token}`,
   },
